00baea5cc5784dcbea52bbb6aa97c08b16d35c85
lss <suiyiaoxiang@outlook.com>
2018-11-28 17:42:21 +0800

    初始化页面

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var swig = require('swig');
 
+// 配置静态资源的访问路径
 app.use(express.static('public'));
+app.set('views','./views/');
 
 // 配置swig
 app.set('view engine','html');
@@ -10,7 +12,9 @@ app.engine('html', swig.renderFile);
 
 // 配置路由
 app.get('/', function (req, res, next) {
-  res.render('index');
+  res.render('index', {
+    title: '首页'
+  });
 });
 
 // 容错机制
@@ -23,6 +27,7 @@ app.use(function (err, req, res, next) {
   res.status(500).send('error...');
 });
 
-app.listen(3000, function () {
+// 启动端口
+app.listen(8088, function () {
   console.log('Server is running...');
 });
